2193e30dc5eb369dfbc9897c88c03095b78c6b18
Maksym <maxim.it.ua@gmail.com>
2019-09-11 15:46:43 +0300

    replacing table with flex in progress

--- src/components/Flow.vue
+++ src/components/Flow.vue
@@ -1,12 +1,18 @@
 <template lang="pug">
 div.flow-grid.mode-edit
-  table(v-if="grid")
-    tr(v-for="(row, rowInd) in grid")
-      td(v-for="(col, colInd) in row")
-        div(v-if="rowInd % 2 === 1 && colInd % 2 === 1" style="display: flex; flex-direction:column;")
-          component(v-for="(elem, elemInd) in col" :key="elemInd" :is="getElementComponent(elem.type)" v-model="col[elemInd]")
-        div(v-else)
-          component(v-for="(elem, elemInd) in col" :key="elemInd" :is="getElementComponent(elem.type)" v-model="col[elemInd]")
+
+  .row(v-for="(row, rowInd) in grid", style="display: flex; flex-direction: row nowrap;")
+    .col(v-for="(col, colInd) in row", style="width: 200px; min-height: 20px; flex-direction: column; position: relative;")
+      component(v-for="(elem, elemInd) in col" :key="elemInd" :is="getElementComponent(elem.type)" v-model="col[elemInd]")
+  //- div(v-if="grid") {{columnWidth}}
+  //- table(v-if="grid")
+  //-   tr(v-for="(row, rowInd) in grid")
+  //-     td(v-for="(col, colInd) in row")
+  //-       //- div(v-if="rowInd % 2 === 1 && colInd % 2 === 1" style="display: flex; flex-direction:column;")
+  //-       div(v-if="rowInd % 2 === 1 && colInd % 2 === 1" style="height: 100%;")
+  //-         component(v-for="(elem, elemInd) in col" :key="elemInd" :is="getElementComponent(elem.type)" v-model="col[elemInd]")
+  //-       div(v-else)
+  //-         component(v-for="(elem, elemInd) in col" :key="elemInd" :is="getElementComponent(elem.type)" v-model="col[elemInd]")
 </template>
 
 <script>
@@ -35,6 +41,20 @@ export default {
         a[c.name] = c
         return a
       }, this.$options.components)
+    },
+    columnWidth() {
+      let extenders = Array(this.grid[0].length).fill(0)
+      for (let [rowInd, row] of this.grid.entries()) {
+        //console.log(row)
+        for (let [colInd, cell] of row.entries()) {
+          if (colInd % 2 === 1 && rowInd % 2 === 1) continue
+          const cellMax = cell.reduce((max, elem) => {
+            return max > elem.padLeft ? max : elem.padLeft  
+          }, 0)
+          extenders[colInd] = cellMax > extenders[colInd] ? cellMax : extenders[colInd]
+        }
+      }
+      return extenders
     }
   },
   methods: {
